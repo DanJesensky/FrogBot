99e74bfaba31668f96f299a92e6330a2cf7b4256
Dan Jesensky <5432700+DanJesensky@users.noreply.github.com>
2021-09-28 22:40:53 -0400

    feat: add --migrate flag to update db and quit

--- FrogBot/Program.cs
+++ FrogBot/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using FrogBot.ChatCommands;
 using FrogBot.Responders;
@@ -22,13 +23,12 @@ namespace FrogBot {
             var host = CreateHostBuilder(args).Build();
             var services = host.Services;
 
-            try
+            if (args.Any(arg => arg.Equals("--migrate")))
             {
                 await using var db = services.GetRequiredService<VoteDbContext>();
-                await db.Database.EnsureCreatedAsync();
                 await db.Database.MigrateAsync();
+                return;
             }
-            catch { }
 
             var client = services.GetRequiredService<DiscordGatewayClient>();
             var cancellationSource = new CancellationTokenSource();
