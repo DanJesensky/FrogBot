a125f1e6dd4cc6c2d98c0316e7a71be7596ddc4f
Dan Jesensky <5432700+DanJesensky@users.noreply.github.com>
2025-12-25 16:49:14 -0500

    fix: add missing migration

--- FrogBot/Migrations/20251225214831_AddUsernameLengthConstraint.Designer.cs
+++ FrogBot/Migrations/20251225214831_AddUsernameLengthConstraint.Designer.cs
@@ -2,6 +2,7 @@
 using FrogBot.Voting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FrogBot.Migrations
 {
     [DbContext(typeof(VoteDbContext))]
-    partial class VoteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251225214831_AddUsernameLengthConstraint")]
+    partial class AddUsernameLengthConstraint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
